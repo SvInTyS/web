e92df1e8afb93c2bd34cf70d3fd66df6a5bfb5e7
SvInTyS <an.al.zaitsev@yandex.ru>
2025-06-07 08:42:57 +0300

    Добавляем геттеры в Item.java

--- src/model/Item.java
+++ src/model/Item.java
@@ -1,9 +1,46 @@
 package model;
 
 public class Item {
-    public int id;
-    public String name;
-    public String description;
-    public double price;
-    public String image;
+    private int id;
+    private String name;
+    private String description;
+    private double price;
+    private String image;
+
+    // Конструктор по умолчанию
+    public Item() {}
+
+    // Геттеры
+    public int getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public String getImage() {
+        return image;
+    }
+
+    // Сеттеры (на будущее, если будешь добавлять/изменять товары)
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    public void setImage(String image) {
+        this.image = image;
+    }
 }
